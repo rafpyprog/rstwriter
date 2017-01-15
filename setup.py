798a932d90a8b66b5bd1a2151f5aa041868b1683
Rafael Alves Ribeiro <rafael.alves.ribeiro@gmail.com>
2017-01-15 11:29:43 -0200

    packing

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name = 'rstwriter',
     packages = ['rstwriter'],
-    version = '0.1',
+    version = '0.3',
     description = 'A report writer powered with reStructuredText for Pandas library.',
     author = 'Rafael Alves Ribeiro',
     author_email = 'rafael.alves.ribeiro@gmail.com',
     url = 'https://github.com/rafpyprog/rstwriter',
-    download_url = 'https://github.com/rafpyprog/rstwriter/tarball/0.1',
+    download_url = 'https://github.com/rafpyprog/rstwriter/tarball/0.3',
     keywords = ['reStructuredText', 'rst', 'pandas', 'reporting'],
     classifiers = [],
                             )
