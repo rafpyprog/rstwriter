7a68d3f5fa28ec659e26a6513711a8cad9f4edc6
Rafael Alves Ribeiro <rafael.alves.ribeiro@gmail.com>
2017-01-16 12:41:11 -0200

    'tests'

--- setup.py
+++ setup.py
@@ -7,17 +7,13 @@ setup(
     name='rstwriter',
     packages=find_packages(exclude=['contrib', 'docs', 'tests', 'dist']),
     install_requires=['docutils'],
-    version='0.8.4',
+    version='0.8.5',
     description='A report writer powered with reStructuredText for Pandas library.',
     author='Rafael Alves Ribeiro',
     author_email='rafael.alves.ribeiro@gmail.com',
     license='MIT',
     url='https://github.com/rafpyprog/rstwriter',
-    download_url='https://github.com/rafpyprog/rstwriter/tarball/0.8.4',
+    download_url='https://github.com/rafpyprog/rstwriter/tarball/0.8.5',
     keywords=['reStructuredText', 'rst', 'pandas', 'reporting'],
-    classifiers=(
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Science/Research',
-        'Programming Language :: Python :: 3.5'.
-    ),
+    classifiers=[],
 )
